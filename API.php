af98be2c8dd21c50a17af58d23254ba5d6d62167
Dominik Szczepański <163560221+Dominik-developer@users.noreply.github.com>
2024-08-17 21:55:25 +0200

    Add files via upload

--- API.php
+++ API.php
@@ -1,21 +1,23 @@
 <?php
-/* API for calculator */
+
 
 header("Content-Type: application/json");
 
 function calculateCompoundInterest($principal, $rate, $time) {
-    $result = [];
+    $results = [];
 
     //principal = kwota
-    for ($year =1; $year <= $time; $year++) {
-        $amount = $principal * pow((1+$rate/100), $year); 
-        $result = array("year" => $year, "amount" => round($amount, 2)); 
+    for ($year = 1; $year <= $time; $year++) {
+        $amount = $principal * pow((1 + $rate / 100), $year); //pierwszy argument do potęgi drugiego
+        $results[] = array("year" => $year, "amount" => round($amount, 2)); //tablica asocjacyjna
     }
 
-    return $result;
+    return $results;
 }
-    $principal = $_GET["principal"] ?? 0; 
+
+    $principal = $_GET["principal"] ?? 0;
     $rate = $_GET["rate"] ?? 0;
     $time = $_GET["time"] ?? 0;
 
     echo json_encode(calculateCompoundInterest($principal, $rate, $time));
+
